df0d7529d2ea2e80d42613e89a575b1b33241aab
root <admin@example.com>
2018-03-27 10:31:09 -0400

    initial checkin

---
+++ CarApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IssueAsset()
+        {
+            ViewBag.Message = "Add options to issue an asset";
+
+            return View();
+        }
+
+        public ActionResult ReceiveAsset()
+        {
+            ViewBag.Message = "Add options to receive an asset";
+
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            ViewBag.Message = "Displays list of users";
+
+            return View();
+        }
+
+        public ActionResult Assets()
+        {
+            ViewBag.Message = "Display list of assets";
+
+            return View();
+        }
+
+        public ActionResult Centers()
+        {
+            ViewBag.Message = "Display list of Centers";
+
+            return View();
+        }
+
+        public ActionResult Apos()
+        {
+            ViewBag.Message = "Display list of APOs";
+
+            return View();
+        }
+    }
+}
---
+++ CarServices/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CarServices
+{
+    public class Class1
+    {
+    }
+}
