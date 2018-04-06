9183ed2f538cc80bce1c7fe9fb05680cc368e2f9
amarbrar <yamlajatt22@gmail.com>
2018-04-06 08:59:50 -0400

    Adding grid to Assets page and showing dummy data

---
+++ CarApp/Controllers/AssetController.cs
@@ -0,0 +1,48 @@
+﻿using CarApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CarApp.Controllers
+{
+    public class AssetController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Asset> Get()
+        {
+            var assetList = new List<Asset>()
+            {
+                new Asset{Barcode = 1234567, DeviceName = "Device 1", Manufacturer = "Dell"},
+                new Asset{Barcode = 2345678, DeviceName = "Device 2", Manufacturer = "Lenovo"},
+                new Asset{Barcode = 3456789, DeviceName = "Device 3", Manufacturer = "Apple"},
+                new Asset{Barcode = 4567890, DeviceName = "Device 4", Manufacturer = "IBM"}
+            };
+
+            return assetList;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Update([FromBody]Asset value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- CarApp/Controllers/HomeController.cs
+++ CarApp/Controllers/HomeController.cs
@@ -38,8 +38,15 @@ namespace CarApp.Controllers
         public ActionResult Assets()
         {
             ViewBag.Message = "Display list of assets";
-
-            return View();
+            
+            var assetList = new List<Asset>()
+            {
+                new Asset{Barcode = 1234567, DeviceName = "Device 1", Manufacturer = "Dell"},
+                new Asset{Barcode = 2345678, DeviceName = "Device 2", Manufacturer = "Lenovo"},
+                new Asset{Barcode = 3456789, DeviceName = "Device 3", Manufacturer = "Apple"},
+                new Asset{Barcode = 4567890, DeviceName = "Device 4", Manufacturer = "IBM"}
+            };
+            return View(assetList);
         }
 
         public ActionResult Centers()
@@ -54,6 +61,12 @@ namespace CarApp.Controllers
             ViewBag.Message = "Display list of APOs";
 
             return View();
+        }
+
+
+        public void AddAsset()
+        {
+            var assetList = ViewBag.Assets;
         }
 
         [HttpPost]
